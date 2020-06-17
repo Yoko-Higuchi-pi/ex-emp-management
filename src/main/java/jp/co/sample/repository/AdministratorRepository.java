f80cf0bba28fac92b36d2032d9847f8a9cd3ebd1
yoko-higuchi <yoko.higuchi@rakus.co.jp>
2020-06-17 11:38:24 +0900

    upload Repository(repositoryパッケージの作成)
    upload EmployeeRepository.java(RepositoryパッケージのEmployeeRepositoryを作成)
    
    upload AdministratorRepository(Repositoryパッケージのクラス作成)

--- src/main/java/jp/co/sample/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -1,11 +1,63 @@
 package jp.co.sample.repository;
 
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import jp.co.sample.domain.Administrator;
+
 /**
- * administrators テーブル操作
+ * administrators テーブル操作を行うリポジトリ
  * 
  * @author yoko.higuchi
  *
  */
 public class AdministratorRepository {
-
+	/** administrators の RowMapper */
+	private static final RowMapper<Administrator> ADMI_ROW_MAPPER = (rs, i) -> {
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("passrowd"));
+		
+		return administrator;
+	};
+	
+	/** SQL実行を行う */
+	NamedParameterJdbcTemplate template;
+	
+	/**
+	 * INSERT 実行を行う
+	 * 
+	 * @param administrator 挿入するデータ
+	 */
+	public void insert(Administrator administrator) {
+		String sql = "INSERT INTO administrators(name, mail_address, password) VALUES(:name, :mailAddress, :password);";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+		
+		template.update(sql, param);
+	}
+	
+	/**
+	 * mail_address, password から該当するデータを抽出する
+	 * 
+	 * @param mailAddress 検索したいメールアドレス
+	 * @param password 検索したいパスワード
+	 * @return 検索結果(ヒットしたデータ)
+	 */
+	public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
+		String sql = "SELECT id, name, mail_address, password FROM administrators WHERE mail_address=:mailAddress AND password=:password;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
+		
+		// 該当しない場合は null を返す
+		try {
+			return template.queryForObject(sql, param, ADMI_ROW_MAPPER);
+		}catch (Exception e) {
+			return null;
+		}
+		
+	}
 }
--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -1,11 +1,91 @@
 package jp.co.sample.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
 /**
- * employees テーブル操作
+ * employees テーブル操作を行うリポジトリ
  * 
  * @author yoko.higuchi
  *
  */
-public class EmployeeRepository {
 
+@Repository
+public class EmployeeRepository {
+	/** EmployeeのRowMapper */
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+		Employee employee = new Employee();
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getDate("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address"));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("dependents_count"));
+		
+		return employee;
+	};
+	
+	/** SQL実行変数 */
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/**
+	 * 全検索を行う
+	 * @return 全件のリスト
+	 */
+	public List<Employee> findAll() {
+		List<Employee> employeeList = new ArrayList<>();
+		String sql = "SELECT id, name, image, gender, hire_date, mail_address, zip_code, address, telephone, salary, characteristics, dependents_count FROM employees ORDER BY hire_date;";
+		try {
+			employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+			return employeeList;			
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * id をもとにDB検索を行う
+	 * @param id 検索したい主キー(id)
+	 * @return 検索結果
+	 */
+	public Employee load(Integer id) {
+		Employee employee = new Employee();
+		String sql = "SELECT id, name, image, gender, hire_date, mail_address, zip_code, address, telephone, salary, characteristics, dependents_count FROM employees WHERE id = :id;";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		try {
+			employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+			return employee;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * id に該当するデータを更新
+	 * @param employee 更新するデータの情報
+	 */
+	public void update(Employee employee) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		String sql = "UPDATE employees SET name=:name, image=:image, gender=:gender, hire_date=:hireDate, mail_address=:mailAddress, zip_code=:zipCode, address=:address, telephone=:telephone, salary=:salary, characteristics=:characteristics, dependents_count=:dependentsCount WHERE id = :id;";
+		
+		template.update(sql, param);
+	}
 }
