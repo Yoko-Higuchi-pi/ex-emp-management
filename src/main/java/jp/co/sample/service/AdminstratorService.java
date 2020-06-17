3a2e27bceed44f6c697e98cf3cce47072733198d
yoko-higuchi <yoko.higuchi@rakus.co.jp>
2020-06-17 12:02:40 +0900

    upload Administrator Form and Service(administrator のformとserviceパッケージを作成)

--- src/main/java/jp/co/sample/service/AdminstratorService.java
+++ src/main/java/jp/co/sample/service/AdminstratorService.java
@@ -1,11 +1,22 @@
 package jp.co.sample.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.repository.AdministratorRepository;
+
 /**
  * 管理者関連の業務処理を行うサービス
  * 
  * @author yoko.higuchi
  *
  */
-public class AdminstratorService {
 
+@Service
+@Transactional
+public class AdminstratorService {
+	@Autowired
+	private AdministratorRepository administratorRepository;
+	
 }
