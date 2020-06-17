b1f1c7620a2b85dd7872ef8cc15f1c376a2eb4c4
yoko-higuchi <yoko.higuchi@rakus.co.jp>
2020-06-17 13:52:00 +0900

    upload AdministratorController(Controllerパッケージの登録機能追加)

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -36,8 +39,18 @@ public class AdministratorController {
 	 * insert を行う HTML を返す.
 	 * @return insert を行う
 	 */
-	@RequestMapping("/toInsert")
+	@RequestMapping("/insert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	@RequestMapping("/toInsert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		
+		return "redirect:/";
 	}
 }
